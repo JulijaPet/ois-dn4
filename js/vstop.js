a0b7e8989d859ff273e55499b407007c656e43ec
Julija Petric <julija.petric@gmail.com>
2014-12-07 18:50:19 +0000

    poskus

--- js/vstop.js
+++ js/vstop.js
@@ -1,10 +1,19 @@
-<input id="btntest" type="button" value="Check" 
-       onclick="return btntest_onclick()" />
-<script language="javascript" type="text/javascript">
+var baseUrl = 'https://rest.ehrscape.com/rest/v1';
+var queryUrl = baseUrl + '/query';
 
-function btntest_onclick() 
-{
-    window.location.href = "http://www.google.com";
+var username = "ois.seminar";
+var password = "ois4fri";
+
+function getSessionId() {
+    var response = $.ajax({
+        type: "POST",
+        url: baseUrl + "/session?username=" + encodeURIComponent(username) +
+                "&password=" + encodeURIComponent(password),
+        async: false
+    });
+    return response.responseJSON.sessionId;
 }
-
-</script>
+function uporabnik() {
+    $("#home").show();
+    $("#zacetnaStran").hide();
+}
