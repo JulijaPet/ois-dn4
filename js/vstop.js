fe4a9e2138149469ad810202193a3e6df26c60ae
Julija Petric <julija.petric@gmail.com>
2014-12-09 21:14:21 +0000

    poskus

--- js/vstop.js
+++ js/vstop.js
@@ -23,11 +23,12 @@ function uporabnik() {
 function skrij() {
     $("#slide").hide();
     $("#mobi").show();
+    document.getElementById("desno").style.width="90%";
 }
 function prikazi() {
     $("#mobi").hide();
     $("#slide").show();
-    document.getElementById("desno").setAttribute("class","wide");
+    document.getElementById("desno").style.width="50%";
 }
 function moznosti() {
     if(index === 0) {
