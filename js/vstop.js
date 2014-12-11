acc3d59e4813d02e8938973b28883a685762e23e
Julija Petric <julija.petric@gmail.com>
2014-12-11 21:07:35 +0000

    poskus

--- js/vstop.js
+++ js/vstop.js
@@ -1,5 +1,3 @@
-var index = 0;
-var id = 0;
 
 function odjava() {
     $("#zacetnaStran").show();
@@ -9,32 +7,7 @@ function odjava() {
     $("#uporabnikImeInPriimek").hide();
     $("#prikatiSkrij").hide();
 }
-function uporabnik() {
-    $("#zacetnaStran").hide();
-    $("#prijavaZac").hide();
-    $("#preberiPredlogoBolnika").hide();
-    $("#home").show();
-    $("#uporabnikImeInPriimek").show();
-    $("#prikatiSkrij").show();
-}
 
-function moznosti() {
-    if(index === 0) {
-        $("#teza1").show();
-        $("#visina1").show();
-        $("#temperatura1").show();
-        $("#krvniTlak1").show();
-        $("#kisik1").show();
-        index = 1;
-    } else {
-        $("#teza1").hide();
-        $("#visina1").hide();
-        $("#temperatura1").hide();
-        $("#krvniTlak1").hide();
-        $("#kisik1").hide();
-        index = 0;
-    }
-}
 function prikaziTeza() {
     $("#polje2").hide();
     $("#row2").show();
@@ -42,7 +15,7 @@ function prikaziTeza() {
     $("#kajVnesujes").html("<span>telesna masa</span>");
     $("#mera1").html("<span>kg</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziVis() {
     $("#polje2").hide();
@@ -51,7 +24,7 @@ function prikaziVis() {
     $("#kajVnesujes").html("<span>telesna višina</span>");
     $("#mera1").html("<span>cm</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziTem() {
     $("#polje2").hide();
@@ -60,7 +33,7 @@ function prikaziTem() {
     $("#kajVnesujes").html("<span>telesna temperatura</span>");
     $("#mera1").html("<span>°C</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziTlak() {
     $("#polje2").show();
@@ -71,7 +44,7 @@ function prikaziTlak() {
     $("#kajVnesujes2").html("<span>diastolični krvni tlak</span>");
     $("#mera2").html("<span>mm Hg</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziKisik() {
     $("#polje2").hide();
@@ -80,7 +53,7 @@ function prikaziKisik() {
     $("#kajVnesujes").html("<span>nasičenost krvi s kisikom</span>");
     $("#mera1").html("<span>%</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziVoda() {
     $("#polje2").hide();
@@ -89,12 +62,12 @@ function prikaziVoda() {
     $("#kajVnesujes").html("<span>voda</span>");
     $("#mera1").html("<span>dl</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziVaje() {
     $("#row2").hide();
     $("#humanBody").show();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziDan() {
     $("#vitalniZnakiPlosca").show();
@@ -105,12 +78,11 @@ function prikaziDan() {
     $("#kajVnesujes2").html("<span>kolicina</span>");
     $("#mera2").html("<span>kos-ov</span>");
     $("#humanBody").hide();
-    $("#telovadnice").hide();
+    $("#map-canvas").hide();
 }
 function prikaziTel() {
     $("#row2").hide();
     $("#humanBody").hide();
-    $("#telovadnice").show();
     $("#map-canvas").show();
 }
 function domov() {
