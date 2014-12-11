4b4c68ab02df1d70c657049ebc8cce92074f4252
Julija Petric <julija.petric@gmail.com>
2014-12-11 14:46:24 +0000

    poskus

--- js/funkcije.js
+++ js/funkcije.js
@@ -65,4 +65,13 @@ function uporabnik() {
 		    }
 		});
 	}
-}
+}/*
+$(document).ready(function() {
+	$('#preberiPredlogoBolnika').change(function() {
+		$("#kreirajSporocilo").html("");
+		var podatki = $(this).val().split(",");
+		$("#kreirajIme").val(podatki[0]);
+		$("#kreirajPriimek").val(podatki[1]);
+		$("#kreirajDatumRojstva").val(podatki[2]);
+	});
+});*/
