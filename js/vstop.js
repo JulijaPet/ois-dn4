d5ddac43102a3c64e891cca050a96b6d35522132
Julija Petric <julija.petric@gmail.com>
2014-12-11 19:08:51 +0000

    poskus

--- js/vstop.js
+++ js/vstop.js
@@ -37,6 +37,7 @@ function moznosti() {
 }
 function prikaziTeza() {
     document.getElementById("#polje2").style.display = "none";
+    $("#row2").show();
     $("#kajVnesujes").html("<span class='label label-default'>'telesna masa'</span>");
     $("#mera1").html("<span class='input-group-addon'>'KG'</span>");
 }
